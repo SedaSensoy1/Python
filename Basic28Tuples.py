bc341e8489c20cbf336652ef665cee9482997470
SedaSensoy1 <sensoysedaa@gmail.com>
2023-11-30 20:46:05 +0100

    Update Basic28Tuples.py

--- Basic28Tuples.py
+++ Basic28Tuples.py
@@ -1,10 +1,24 @@
 #TUPLES: 
 # Tuples are containers like lists with the difference being that they are immutable(Elements cannot be changed after being defined)
 
-tuple1 = ("a", "b", "c")
-print(tuple1)
-print(tuple1[2]) #c
-print(tuple1[1:])
+# tuple1 = ("a", "b", "c")
+# print(tuple1)
+# print(tuple1[2]) #c
+# print(tuple1[1:])
 
-tuple2 = 1, 2, 3
-print(tuple2)
+# tuple2 = 1, 2, 3
+# print(tuple2)
+#############################################################
+# Creating a tuple using parentheses
+tuple_with_parentheses = (1, 2, 3)
+
+# Creating a tuple without using parentheses
+tuple_without_parentheses = 4, 5, 6
+
+# If you're creating a tuple with only one element, you need to use a comma
+single_element_tuple = 7,
+
+# Printing the tuples
+print(tuple_with_parentheses)    # (1, 2, 3)
+print(tuple_without_parentheses) # (4, 5, 6)
+print(single_element_tuple)      # (7,)
