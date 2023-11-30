701c6a54535bd34ec86aaaf322d3f473fabd4ef6
SedaSensoy1 <sensoysedaa@gmail.com>
2023-11-30 20:41:17 +0100

    Create Basic28Tuples.py

---
+++ Basic28Tuples.py
@@ -0,0 +1,10 @@
+#TUPLES: 
+# Tuples are containers like lists with the difference being that they are immutable(Elements cannot be changed after being defined)
+
+tuple1 = ("a", "b", "c")
+print(tuple1)
+print(tuple1[2]) #c
+print(tuple1[1:])
+
+tuple2 = 1, 2, 3
+print(tuple2)
