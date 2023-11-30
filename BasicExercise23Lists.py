3655f731f62f83302465364170e427ff4ee8e21a
SedaSensoy1 <sensoysedaa@gmail.com>
2023-11-30 20:21:07 +0100

    Update BasicExercise23Lists.py

--- BasicExercise23Lists.py
+++ BasicExercise23Lists.py
@@ -1,1 +1,18 @@
 #Example programs to illustrate Lists in
+
+List = [1,2,3]
+print(List)
+print(len(List))
+
+List = [1.34, 2.45, 7.89, 6.54]
+print(List)
+print(len(List))
+
+List = ["Hello", "world", "welcome", "to", "python"]
+print(List)
+print(len(List))
+
+List = [1, 1.45, "hello", 2.89, 7, "world"]
+print(List)
+print(len(List))
+print(List[2])
