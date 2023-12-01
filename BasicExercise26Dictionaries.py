9a9b1756402659b9083b268d3b2c516a97ea9ced
SedaSensoy1 <sensoysedaa@gmail.com>
2023-12-01 19:56:49 +0100

    Create BasicExercise26Dictionaries.py

---
+++ BasicExercise26Dictionaries.py
@@ -0,0 +1,7 @@
+#Write a Python program to print all the items in a dictionary
+dict = {'Seda':['student','seda@mail.com'],
+        'Eda':['student']}
+
+for k,v in dict.items():
+
+     print(k, ":", v)
