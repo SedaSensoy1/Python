c9633a3798d3c713c00969c0bdffd22b14ce48d5
SedaSensoy1 <sensoysedaa@gmail.com>
2023-12-08 21:15:55 +0100

    Create Basic34Strings.py

---
+++ Basic34Strings.py
@@ -0,0 +1,30 @@
+# message = "Hello!"
+# index = 0
+# for i in message: 
+#     print("message[",index,"]=",i)
+#     index += 1
+######################################
+# message1 = "Hello!"
+# message2 = "World!!"
+# for i in message1:
+#     print(i)
+# for i in message2: 
+#     print(i)
+#####################################
+# str1 = "Hello, welcome to Python"
+# i = 0
+# print(str1[i])
+# print(str1[i*3+1])
+#####################################
+# str1 = "Hello "
+# str2 = "World"
+# str3 = str1 + str2
+# print("The concatenated",str3)
+####################################
+# str1 = "Hello "
+# print(str1 * 3)
+####################################
+# str1 = "Hello "
+# var = 7
+# str2 = str1 + str(var)
+# print(str2)
