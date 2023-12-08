af57dec816122acbc5f82c61a9c7bdc3c3508907
SedaSensoy1 <sensoysedaa@gmail.com>
2023-12-08 22:00:09 +0100

    Create Basic35String_built_in_functions.py

---
+++ Basic35String_built_in_functions.py
@@ -0,0 +1,10 @@
+# str1 = "Hello"
+# print("Length: ",len(str1))
+##################################
+str1 = "Hello123"
+str2= "aaa"
+str3 = "3"
+#isalnum() -> check if at least one character is a number or an alphabet
+print(str1.isalnum())
+print(str2.isalpha()) #string is alphabet only
+print(str3.isdigit())
